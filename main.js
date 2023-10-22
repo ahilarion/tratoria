eb648683720481873fd7fe8e3947dbd74b36869f
Alan <alan.hilarion@proton.me>
2023-10-23 00:00:05 +0200

    Update : animation

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ function scrollToSection(sectionId) {
         });
     }
 }
+
+function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scale-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+// Créez un observateur d'intersection
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2, // Ajustez le seuil selon vos besoins
+};
+
+const observer = new IntersectionObserver(handleIntersection, options);
+
+// Sélectionnez tous les éléments avec la classe "animate"
+const elements = document.querySelectorAll('.animate');
+
+// Observez chaque élément
+elements.forEach((element) => {
+    observer.observe(element);
+});
