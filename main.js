218dc9fdf208e38968d07f8bed6b0f296e3245b3
Alan <alan.hilarion@proton.me>
2023-10-22 22:24:51 +0200

    Update : Minify files

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-function scrollToSection(sectionId) {
-    const section = document.querySelector(sectionId);
-
-    if (section) {
-        const headerHeight = document.querySelector('header').offsetHeight;
-        const targetPosition = section.offsetTop - headerHeight;
-
-        window.scrollTo({
-            top: targetPosition,
-            behavior: "smooth"
-        });
-    }
-}
+function scrollToSection(sectionId){const section=document.querySelector(sectionId); if (section){const headerHeight=document.querySelector('header').offsetHeight; const targetPosition=section.offsetTop - headerHeight; window.scrollTo({top: targetPosition, behavior: "smooth"});}}
